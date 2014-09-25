66e9ffe598aa55c93cf21f0046917f9331cae6e5
Sam Kennedy <skennedy@mines.edu>
2014-09-24 22:52:59 -0600

    Added empty variables and functions, 2 failing tests written

--- src/IntBoardTest.java
+++ src/IntBoardTest.java
@@ -1,13 +1,38 @@
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class IntBoardTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	IntBoard board;
+	
+	@Before
+	public void setUp(){
+		board = new IntBoard();
 	}
-
+	
+	@Test
+	public void testAdj0(){
+		BoardCell cell = board.getCell(0, 0);
+		LinkedList<BoardCell> testList = board.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(1,0)));
+		Assert.assertTrue(testList.contains(board.getCell(0,1)));
+		Assert.assertEquals(2,testList.size());
+	}
+	
+	@Test
+	public void testAdj1(){
+		BoardCell cell = board.getCell(1, 1);
+		LinkedList<BoardCell> testList = board.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(1,0)));
+		Assert.assertTrue(testList.contains(board.getCell(0,1)));
+		Assert.assertTrue(testList.contains(board.getCell(2,1)));
+		Assert.assertTrue(testList.contains(board.getCell(1,2)));
+		Assert.assertEquals(4,testList.size());
+	}
 }
